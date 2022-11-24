856fb4b3a4bae2f3aad67fb0abb590a52142d41b
unknown <you@example.com>
2022-11-24 12:00:39 +0600

    navbar added

---
+++ src/Router/Router/Router.js
@@ -0,0 +1,36 @@
+import Main from "../../Layout/Main/Main";
+import Blog from "../../Pages/Blog/Blog";
+import Home from "../../Pages/Home/Home/Home";
+import Login from "../../Pages/Login/Login";
+import Register from "../../Pages/Register/Register";
+
+const { createBrowserRouter } = require("react-router-dom");
+
+
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+       element: <Main></Main>,
+       children: [
+        {
+            path: '/',
+            element: <Home></Home>
+        },
+        {
+            path: '/login',
+            element: <Login></Login>
+        },
+        {
+            path: '/register',
+            element: <Register></Register>
+        },
+        {
+            path: '/blog',
+            element: <Blog></Blog>
+        }
+       ]
+
+    }
+
+])
